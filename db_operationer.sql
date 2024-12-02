395a98cb7dc5f9621d0acde7d3c226516c51a0f1
Valter Ekholm <valterekholm@hotmail.com>
2024-12-02 12:46:57 +0100

    Update db_operationer.sql

--- db_operationer.sql
+++ db_operationer.sql
@@ -1,2 +1,5 @@
 --20241126
 ALTER TABLE `faktura` CHANGE `nr` `nr` INT(11) UNSIGNED NOT NULL;
+
+--20241202
+ALTER TABLE `faktura` CHANGE `kommentarer` `kommentarer` VARCHAR(300) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT 'Fyll i datum för arbetet';
